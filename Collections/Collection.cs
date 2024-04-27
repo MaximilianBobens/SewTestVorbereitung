9e5d21d85403272ffa70d210d3ffc7d01f29525a
maxim <m.bobens@htlkrems.at>
2024-04-27 11:37:10 +0200

    initial commit

--- Collections/Collection.cs
+++ Collections/Collection.cs
@@ -2,93 +2,99 @@
 
 class Program
 {
-    static void Collections(string[] args)
+    static void Main(string[] args)
     {
-        /*--------------------------------------------------------------------------------------------*/
-        // Queue erstellen und testen
-        Queue<int> queue = new Queue<int>();
-        for (int i = 1; i <= 10; i++)
+      //PrintQueue();
+      //PrintList();
+      //PrintStack();
+      //PrintDictionary();
+        void PrintList()
         {
-            queue.Enqueue(i);
-        }
-        
-        foreach (int item in queue)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("\nSuche in Queue: " + (queue.Contains(5) ? "gefunden" : "nicht gefunden"));
-        int dequeueItem = queue.Dequeue();
-        Console.WriteLine("Element aus der Queue entfernt: " + dequeueItem);
-        Console.WriteLine("Queue nach Entfernen:");
-        foreach (int item in queue)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("\n");
-/*--------------------------------------------------------------------------------------------*/
-        // Stack erstellen und testen
-        Stack<int> stack = new Stack<int>();
-        for (int i = 1; i <= 10; i++)
-        {
-            stack.Push(i);
-        }
-        
-        foreach (int item in stack)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("\nSuche im Stack: " + (stack.Contains(5) ? "gefunden" : "nicht gefunden"));
-        int popItem = stack.Pop();
-        Console.WriteLine("Element aus dem Stack entfernt: " + popItem);
-        Console.WriteLine("Stack nach Entfernen:");
-        foreach (int item in stack)
-        {
-            Console.Write(item + " ");
-        }
-        Console.WriteLine("\n");
-/*--------------------------------------------------------------------------------------------*/
-        // List erstellen und testen
-        List<int> list = new List<int>();
-        for (int i = 1; i <= 10; i++)
-        {
-            list.Add(i);
+            List<int> list = new List<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                list.Add(i);
+            }
+
+            if (list.Contains(3))
+            {
+                list.Remove(3);
+            }
+            
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
 
-     
-        foreach (int item in list)
+        void PrintStack()
         {
-            Console.Write(item + " ");
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                stack.Push(i);
+            }
+            
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+            
+            if (stack.Contains(4))
+            {
+                stack.Pop();
+            }
+            
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+          
         }
-        Console.WriteLine("\nSuche in der Liste: " + (list.Contains(5) ? "gefunden" : "nicht gefunden"));
-        list.Remove(5);
-        Console.WriteLine("Element aus der Liste entfernt.");
-        Console.WriteLine("Liste nach Entfernen:");
-        foreach (int item in list)
+
+        void PrintQueue()
         {
-            Console.Write(item + " ");
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i <= 10; i++)
+            {
+                queue.Enqueue(i);
+             
+            }
+            
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
+            
+            if (queue.Contains(3))
+            {
+                queue.Dequeue();
+            }
+            foreach (var item in queue)
+            {
+                Console.WriteLine(item);
+            }
         }
-        Console.WriteLine("\n");
-/*--------------------------------------------------------------------------------------------*/
-        // Dictionary erstellen und testen
-        Dictionary<int, string> dictionary = new Dictionary<int, string>();
-        for (int i = 1; i <= 5; i++)
+
+        void PrintDictionary()
         {
-            dictionary.Add(i, "Value" + i);
+            Dictionary<string, int> books = new Dictionary<string, int>();
+            Random r = new Random();
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                int randomBookNumber = r.Next(100, 500);
+                string bookTitle = string.Format("Test book {0}", i);
+                books.Add(bookTitle, randomBookNumber);
+            }
+
+            foreach (var item in books)
+            {
+                Console.WriteLine(item);
+            }
+            
         }
-        
-        foreach (var kvp in dictionary)
-        {
-            Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
-        }
-        Console.WriteLine("Suche im Dictionary: " + (dictionary.ContainsKey(3) ? "gefunden" : "nicht gefunden"));
-        dictionary.Remove(3);
-        Console.WriteLine("Element aus dem Dictionary entfernt.");
-        Console.WriteLine("Dictionary nach Entfernen:");
-        foreach (var kvp in dictionary)
-        {
-            Console.WriteLine("Key: " + kvp.Key + ", Value: " + kvp.Value);
-        }
-        Console.WriteLine("\n");
+
     }
 }
 
